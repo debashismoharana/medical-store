d7b64fd019e6a263760853b71fbcdf55267b3ec3
Debashis Moharana <debashismoharana@users.noreply.github.com>
2019-10-08 22:16:43 +0530

    added delete medicine and pull to refresh

--- src/app/medicines/medicines.page.ts
+++ src/app/medicines/medicines.page.ts
@@ -10,19 +10,41 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class MedicinesPage implements OnInit {
   private selectedItem: any;
-  public items: Array<{ name: string; price: number }> = [];
+  public items: Array<{ id: string; name: string; price: number }> = [];
   constructor(public modalController: ModalController, public db: AngularFirestore) { }
 
   getMedicines() {
+    this.items = [];
     this.db.collection('medicines').get().subscribe((querySnapshot) => {
       querySnapshot.forEach((medicine) => {
           console.log(medicine.id, ' => ', medicine.data());
           this.items.push({
+              id: medicine.id,
               name: medicine.data().name,
               price: medicine.data().price
             });
       });
     });
+  }
+
+  doRefresh(event) {
+    this.getMedicines();
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
+  deleteMedicine(item) {
+    this.db.collection('medicines').doc(item.id).delete().then(() => {
+      console.log('Document successfully deleted!');
+      const index = this.items.indexOf(item);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+  }).catch((error) => {
+      console.error('Error removing document: ', error);
+  });
   }
 
   async presentModal() {
@@ -32,12 +54,10 @@ export class MedicinesPage implements OnInit {
     return await modal.present();
   }
 
-  ngOnInit() { }
-  ionViewWillEnter() {
-  }
-  ionViewDidEnter() {
+  ngOnInit() {
     this.getMedicines();
   }
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/medicines', JSON.stringify(item)]);
